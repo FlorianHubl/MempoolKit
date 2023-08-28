7c894145f39114569751f5577549de9ede9568e5
Florian Hubl <florian.hubl@ewe.net>
2023-08-28 12:56:39 +0200

    updated readme

--- README.md
+++ README.md
@@ -18,6 +18,9 @@ A Swift Package that enables to get Bitcoin data through a Mempool instance.
 
 ## Documentation
 
+The Package is fully documented in docC. Open the documentation window in xCode to read the documentation.
+There will also be Tutorials in the documentation in the future which explane how to use the package with SwiftUI.
+
 ### Create a Instance
 
 ```swift
@@ -49,7 +52,7 @@ Returns details about difficulty adjustment.
 ### Address
 
 ```swift
-let address = try await mempool.address("1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv")
+let address = try await mempool.address(address: "1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv")
 ```
 
 Returns details about an address.
@@ -57,7 +60,7 @@ Returns details about an address.
 ### Address Transactions
 
 ```swift
-let txs = try await mempool.addressTXS("1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv")
+let txs = try await mempool.addressTXS(address: "1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv")
 ```
 
 Get transaction history for the specified address/scripthash, sorted with newest first. Returns up to 50 mempool transactions plus the first 25 confirmed transactions. You can request more confirmed transactions using addressTXSChain.
@@ -65,7 +68,7 @@ Get transaction history for the specified address/scripthash, sorted with newest
 ### Address Transactions Chain
 
 ```swift
-let txs = try await mempool.addressTXSChain("1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv", lastTXID: "277bbdc3557f163810feea810bf390ed90724ec75de779ab181b865292bb1dc1")
+let txs = try await mempool.addressTXSChain(address: "1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv", lastTXID: "277bbdc3557f163810feea810bf390ed90724ec75de779ab181b865292bb1dc1")
 ```
 
 Get confirmed transaction history for the specified address/scripthash, sorted with newest first. Returns 25 transactions per page. More can be requested by specifying the last txid seen by the previous call.
@@ -75,13 +78,13 @@ Get confirmed transaction history for the specified address/scripthash, sorted w
 Get unconfirmed transaction history for the specified address/scripthash. Returns up to 50 transactions (no paging).
 
 ```swift
-let txsMempool = try await mempool.addressTXSMempool("1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv")
+let txsMempool = try await mempool.addressTXSMempool(address: "1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv")
 ```
 
 ### Address UTXOs
 
 ```swift
-let utxos = try await mempool.addressUTXOs("1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv")
+let utxos = try await mempool.addressUTXOs(address: "1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv")
 ```
 
 Get the list of unspent transaction outputs associated with the address/scripthash.
@@ -89,7 +92,7 @@ Get the list of unspent transaction outputs associated with the address/scriptha
 ### Get Block
 
 ```swift
-let block = try await mempool.block("000000000000000015dc777b3ff2611091336355d3f0ee9766a2cf3be8e4b1ce")
+let block = try await mempool.block(blockHash: "000000000000000015dc777b3ff2611091336355d3f0ee9766a2cf3be8e4b1ce")
 ```
 
 Returns details about a block.
@@ -97,7 +100,7 @@ Returns details about a block.
 ### Hex Block Header
 
 ```swift
-let blockHeader = try await mempool.blockHeader("000000000000000015dc777b3ff2611091336355d3f0ee9766a2cf3be8e4b1ce")
+let blockHeader = try await mempool.blockHeader(blockHash: "000000000000000015dc777b3ff2611091336355d3f0ee9766a2cf3be8e4b1ce")
 ```
 
 Returns the hex-encoded block header.
@@ -105,7 +108,7 @@ Returns the hex-encoded block header.
 ### Get Block Height
 
 ```swift
-let blockHash = try await mempool.blockHeight(800000)
+let blockHash = try await mempool.blockHeight(blockHeight: 800000)
 ```
 
 Returns the hash of the block at the input height.
@@ -113,7 +116,7 @@ Returns the hash of the block at the input height.
 ### Get raw Block
 
 ```swift
-let rawBlock = try await mempool.blockRaw("0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
+let rawBlock = try await mempool.blockRaw(blockHash: "0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
 ```
 
 Returns the raw block representation in Swifts Data Type.
@@ -121,7 +124,7 @@ Returns the raw block representation in Swifts Data Type.
 ### Block Status
 
 ```swift
-let blockStatus = try await mempool.blockStatus("0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
+let blockStatus = try await mempool.blockStatus(blockHash: "0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
 ```
 
 Returns the confirmation status of a block.
@@ -129,7 +132,7 @@ Returns the confirmation status of a block.
 ### Block Tip Height
 
 ```swift
-let blockTipHeight = try await mempool.blockTipHeight("0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
+let blockTipHeight = try await mempool.blockTipHeight()
 ```
 
 Returns the height of the last block.
@@ -137,7 +140,7 @@ Returns the height of the last block.
 ### Block Tip Hash
 
 ```swift
-let blockTipHash = try await mempool.blockTipHash("0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
+let blockTipHash = try await mempool.blockTipHash()
 ```
 
 Returns the hash of the last block.
@@ -161,7 +164,7 @@ Returns a list of all txids in the block.
 ### Block Transaction
 
 ```swift
-let blockTXIDs = try await mempool.blockTXs("0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
+let blockTXIDs = try await mempool.blockTXs(blockHash: "0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
 ```
 
 Returns a list of 25 transactions in the block.
@@ -169,7 +172,7 @@ Returns a list of 25 transactions in the block.
 ### Get Blocks
 
 ```swift
-let blocks = try await mempool.blocks("0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
+let blocks = try await mempool.blocks(blockHash: "0000000000000000000065bda8f8a88f2e1e00d9a6887a43d640e52a4c7660f2")
 ```
 
 Returns details on the past 15 blocks with fee and mining details in extras.
--- Sources/MempoolKit/MempoolKit.swift
+++ Sources/MempoolKit/MempoolKit.swift
@@ -133,72 +133,118 @@ public struct Mempool {
     
     // Addresses
     
-    /// <#Description#>
+    /// Details about an Bitcoin address.
     /// - Parameter address: Bitcoin Address
-    /// - Returns: Details about difficulty adjustment.
-    
+    /// - Returns: Details about an address such as tx_count
     public func address(address: String) async throws -> Address {
         try await request(for: .address, method: .get, type: Address.self, extention: address)
     }
     
+    /// Addresses Transaction History
+    /// - Parameter address: Bitcoin Address
+    /// - Returns: Transaction history for the specified address, sorted with newest first. Returns up to 50 mempool transactions plus the first 25 confirmed transactions. You can request more confirmed transactions using addressTXSChain
     public func addressTXS(address: String) async throws -> Transactions {
         try await request(for: .address, method: .get, type: Transactions.self, extention: "\(address)/txs")
     }
     
+    /// Address Transactions Chain
+    /// - Parameters:
+    ///   - address: Bitcoin Address
+    ///   - lastTXID: Last seen Bitcoin Transaction ID
+    /// - Returns: Confirmed transaction history for the specified address/scripthash, sorted with newest first. Returns 25 transactions per page. More can be requested by specifying the last txid seen by the previous call
     public func addressTXSChain(address: String, lastTXID: String) async throws -> Transactions {
         try await request(for: .address, method: .get, type: Transactions.self, extention: "\(address)/txs/chain/\(lastTXID)")
     }
     
+    /// Address Transactions Mempool
+    /// - Parameter address: Bitcoin Address
+    /// - Returns: Get unconfirmed transaction history for the specified address/scripthash. Returns up to 50 transactions (no paging)
     public func addressTXSMempool(address: String) async throws -> Transactions {
         try await request(for: .address, method: .get, type: Transactions.self, extention: "\(address)/txs/mempool")
     }
     
+    /// Address UTXOs
+    /// - Parameter address: Bitcoin Address
+    /// - Returns: Get the list of unspent transaction outputs associated with the address
     public func addressUTXOs(address: String) async throws -> UTXOs {
         try await request(for: .address, method: .get, type: UTXOs.self, extention: "\(address)/utxo")
     }
     
     // Blocks
     
+    /// Get Block
+    /// - Parameter blockHash: The Blocks Hash or ID
+    /// - Returns: Returns details about a block
     public func block(blockHash: String) async throws -> Block {
         try await request(for: .block, method: .get, type: Block.self, extention: blockHash)
     }
     
+    /// Hex Block Header
+    /// - Parameter blockHash: The Blocks Hash or ID
+    /// - Returns: The hex-encoded block header
     public func blockHeader(blockHash: String) async throws -> String {
         try await request(for: .block, method: .get, type: String.self, extention: "\(blockHash)/header")
     }
     
+    /// Get Block Height
+    /// - Parameter blockHeight: The Height of the Block
+    /// - Returns: The hash of the block at the input height
     public func blockHeight(blockHeight: Int) async throws -> String {
         try await request(for: .blockheight, method: .get, type: String.self, extention: "\(blockHeight)")
     }
     
+    /// Get raw Block
+    /// - Parameter blockHash: The Blocks Hash or ID
+    /// - Returns: The raw block representation in Swifts Data Type
     public func blockRaw(blockHash: String) async throws -> Data {
         try await request(for: .block, method: .get, type: Data.self, extention: "\(blockHash)/raw")
     }
     
+    /// Block Status
+    /// - Parameter blockHash: The Blocks Hash or ID
+    /// - Returns: The confirmation status of a block.
     public func blockStatus(blockHash: String) async throws -> BlockStatus {
         try await request(for: .block, method: .get, type: BlockStatus.self, extention: "\(blockHash)/status")
     }
     
+    /// Block Tip Height
+    /// - Returns: The height of the last block.
     public func blockTipHeight() async throws -> Int {
         try await request(for: .blockTipHeight, method: .get, type: Int.self)
     }
     
+    /// Block Tip Hash
+    /// - Returns: The hash of the last block.
     public func blockTipHash() async throws -> String {
         try await request(for: .blockTipHash, method: .get, type: String.self)
     }
     
+    /// Block Transaction ID
+    /// - Parameters:
+    ///   - blockHash: The Blocks Hash or ID
+    ///   - index: The index of the transaction in the block.
+    /// - Returns: The transaction at index within the specified block.
     public func blockTXID(blockHash: String, index: Int) async throws -> String {
         try await request(for: .block, method: .get, type: String.self, extention: "\(blockHash)/txid/\(index)")
     }
     
+    /// Block Transaction IDs
+    /// - Parameter blockHash: The Blocks Hash or ID
+    /// - Returns: Returns a list of all Transaction IDs in the block.
     public func blockTXIDs(blockHash: String) async throws -> [String] {
         try await request(for: .block, method: .get, type: [String].self, extention: "\(blockHash)/txids")
     }
     
+    /// Block Transaction
+    /// - Parameter blockHash: The Blocks Hash or ID
+    /// - Returns: Returns a list of 25 transactions in the block.
     public func blockTXs(blockHash: String) async throws -> Transactions {
         try await request(for: .block, method: .get, type: Transactions.self, extention: "\(blockHash)/txs")
     }
     
+    /// Get Blocks
+    /// - Parameter blockHeight: The Blocks Hash or ID
+    /// - Returns: Returns details on the past 15 blocks with fee and mining details in extras
     public func blocks(blockHeight: Int) async throws -> String {
         try await request(for: .blocks, method: .get, type: String.self, extention: "\(blockHeight)")
     }
@@ -209,5 +255,14 @@ public struct Mempool {
         try await request(for: .blocks, method: .get, type: Blocks.self, extention: "\(start)/\(end)")
     }
     
+    // Mining
+    
+    public enum MemPoolTime: String {
+        
+    }
+    
+    public func miningPools(blockHeight: Int) async throws -> String {
+        try await request(for: .blocks, method: .get, type: String.self, extention: "\(blockHeight)")
+    }
 }
 
