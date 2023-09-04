fe6cfd96f5bb3756a93db29b160ad5395fb0f7e6
Florian Hubl <florian.hubl@ewe.net>
2023-09-04 12:23:26 +0200

    added more demos

--- Sources/MempoolKit/Models/Address.swift
+++ Sources/MempoolKit/Models/Address.swift
@@ -6,6 +6,8 @@ public struct Address: Codable {
     public let address: String
     public let chain_stats, mempool_stats: Stats
     
+    public static let demo = Address(address: "address", chain_stats: Stats(funded_txo_count: 21, funded_txo_sum: 21, spent_txo_count: 21, spent_txo_sum: 21, tx_count: 21), mempool_stats: Stats(funded_txo_count: 21, funded_txo_sum: 21, spent_txo_count: 21, spent_txo_sum: 21, tx_count: 21))
+    
     public struct Stats: Codable {
         public let funded_txo_count, funded_txo_sum, spent_txo_count, spent_txo_sum: Int
         public let tx_count: Int
--- Sources/MempoolKit/Models/UTXO.swift
+++ Sources/MempoolKit/Models/UTXO.swift
@@ -7,6 +7,8 @@ public struct UTXO: Codable {
     public let status: Status
     public let value: Int
     
+    public static let demo = UTXO(txid: "txid", vout: 21, status: Status(confirmed: true, block_height: 800000, block_hash: "block_hash", block_time: 800000), value: 21)
+    
     public struct Status: Codable {
         public let confirmed: Bool
         public let block_height: Int?
