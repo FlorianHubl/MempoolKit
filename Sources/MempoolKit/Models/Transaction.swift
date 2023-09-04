845ae47294725c4cf11fc55960b35c4cd2624680
Florian Hubl <florian.hubl@ewe.net>
2023-09-04 10:35:42 +0200

    var in Transaction

--- Sources/MempoolKit/Models/Transaction.swift
+++ Sources/MempoolKit/Models/Transaction.swift
@@ -12,16 +12,10 @@ public struct Transaction: Codable {
     public static let demo = Transaction(txid: "txid", version: 1, locktime: 1, vin: [Vin(txid: "txid", vout: 1, prevout: Vout(scriptpubkey: "scriptpubkey", scriptpubkey_asm: "scriptpubkey_asm", scriptpubkey_type: .p2pkh, scriptpubkey_address: "scriptpubkey_address", value: 1), scriptsig: "scriptsig", scriptsig_asm: "scriptsig_asm", witness: ["witness", "witness", "witness"], is_coinbase: false, sequence: 1, inner_redeemscript_asm: "inner_redeemscript_asm", inner_witnessscript_asm: "inner_witnessscript_asm")], vout: [Vout(scriptpubkey: "scriptpubkey", scriptpubkey_asm: "scriptpubkey_asm", scriptpubkey_type: .p2pkh, scriptpubkey_address: "scriptpubkey_address", value: 1)], size: 1, weight: 1, fee: 1, status: .init(confirmed: true, block_height: 21, block_hash: "block_hash", block_time: 21))
     
     public struct Status: Codable {
-        init(confirmed: Bool, block_height: Int?, block_hash: String?, block_time: Int?) {
-            self.confirmed = confirmed
-            self.block_height = block_height
-            self.block_hash = block_hash
-            self.block_time = block_time
-        }
-        public let confirmed: Bool
-        public let block_height: Int?
-        public let block_hash: String?
-        public let block_time: Int?
+        public var confirmed: Bool
+        public var block_height: Int?
+        public var block_hash: String?
+        public var block_time: Int?
     }
     
     public struct Vin: Codable {
