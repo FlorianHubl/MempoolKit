a3b8b0a8b65e7c3f4982e2875e26c10f61714d80
flohubl <flohubl@smail.hs-bremerhaven.de>
2023-10-05 15:29:04 +0200

    init to RecommendedFees

--- Sources/MempoolKit/Models/RecommendedFees.swift
+++ Sources/MempoolKit/Models/RecommendedFees.swift
@@ -3,5 +3,12 @@ import Foundation
 
 public struct RecommendedFees: Codable {
     public let fastestFee, halfHourFee, hourFee, economyFee, minimumFee: Double
+    public init(fastestFee: Double, halfHourFee: Double, hourFee: Double, economyFee: Double, minimumFee: Double) {
+        self.fastestFee = fastestFee
+        self.halfHourFee = halfHourFee
+        self.hourFee = hourFee
+        self.economyFee = economyFee
+        self.minimumFee = minimumFee
+    }
 }
 
