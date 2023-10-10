3a5e840aa8eca57ccd2415305db69bbfb809944f
flohubl <flohubl@smail.hs-bremerhaven.de>
2023-10-10 15:58:52 +0200

    updated

--- Sources/MempoolKit/Models/RecommendedFees.swift
+++ Sources/MempoolKit/Models/RecommendedFees.swift
@@ -3,7 +3,7 @@ import Foundation
 
 public struct RecommendedFees: Codable {
     public let fastestFee, halfHourFee, hourFee, economyFee, minimumFee: Double
-    public let demo = RecommendedFees(fastestFee: 11, halfHourFee: 7, hourFee: 3, economyFee: 2, minimumFee: 1)
+    static public let demo = RecommendedFees(fastestFee: 11, halfHourFee: 7, hourFee: 3, economyFee: 2, minimumFee: 1)
     public init(fastestFee: Double, halfHourFee: Double, hourFee: Double, economyFee: Double, minimumFee: Double) {
         self.fastestFee = fastestFee
         self.halfHourFee = halfHourFee
